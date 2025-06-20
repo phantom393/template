ec597311fdb1a81ad676bd31fa43913571ea06ef
naim <azrul393@gmail.com>
2025-06-20 20:45:28 +0800

    update

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -298,75 +298,33 @@
         <!--begin::Content wrapper-->
         <div class="d-flex flex-column flex-column-fluid">
             <!--begin::Toolbar-->
-            <div id="kt_app_toolbar" class="app-toolbar py-3 py-lg-6">
-                <!--begin::Toolbar container-->
+            <!-- <div id="kt_app_toolbar" class="app-toolbar py-3 py-lg-6">
                 <div id="kt_app_toolbar_container" class="app-container container-fluid d-flex flex-stack">
-                    <!--begin::Page title-->
                     <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
-                        <!--begin::Title-->
-                        <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">Multipurpose</h1>
-                        <!--end::Title-->
-                        <!--begin::Breadcrumb-->
                         <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
-                            <!--begin::Item-->
                             <li class="breadcrumb-item text-muted">
                                 <a href="../../demo1/dist/index.html" class="text-muted text-hover-primary">Home</a>
                             </li>
-                            <!--end::Item-->
-                            <!--begin::Item-->
                             <li class="breadcrumb-item">
                                 <span class="bullet bg-gray-400 w-5px h-2px"></span>
                             </li>
-                            <!--end::Item-->
-                            <!--begin::Item-->
                             <li class="breadcrumb-item text-muted">Dashboards</li>
-                            <!--end::Item-->
                         </ul>
-                        <!--end::Breadcrumb-->
                     </div>
-                    <!--end::Page title-->
-                    <!--begin::Actions-->
-                    
-                    <!--end::Actions-->
                 </div>
-                <!--end::Toolbar container-->
-            </div>
+            </div> -->
             <!--end::Toolbar-->
             <!--begin::Content-->
             <div id="kt_app_content" class="app-content flex-column-fluid">
                 <div class="app-container container-fluid">
-                    @yield('body-content')
+                    @yield('content')
                 </div>
             </div>
             <!--end::Content-->
         </div>
         <!--end::Content wrapper-->
         <!--begin::Footer-->
-        <div id="kt_app_footer" class="app-footer">
-            <!--begin::Footer container-->
-            <div class="app-container container-fluid d-flex flex-column flex-md-row flex-center flex-md-stack py-3">
-                <!--begin::Copyright-->
-                <div class="text-dark order-2 order-md-1">
-                    <span class="text-muted fw-semibold me-1">2023&copy;</span>
-                    <a href="https://keenthemes.com" target="_blank" class="text-gray-800 text-hover-primary">Keenthemes</a>
-                </div>
-                <!--end::Copyright-->
-                <!--begin::Menu-->
-                <ul class="menu menu-gray-600 menu-hover-primary fw-semibold order-1">
-                    <li class="menu-item">
-                        <a href="https://keenthemes.com" target="_blank" class="menu-link px-2">About</a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="https://devs.keenthemes.com" target="_blank" class="menu-link px-2">Support</a>
-                    </li>
-                    <li class="menu-item">
-                        <a href="https://1.envato.market/EA4JP" target="_blank" class="menu-link px-2">Purchase</a>
-                    </li>
-                </ul>
-                <!--end::Menu-->
-            </div>
-            <!--end::Footer container-->
-        </div>
+        @include('layouts.partials.footer')
         <!--end::Footer-->
     </div>
     <!--end:::Main-->
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,7 +1,19 @@
-{{-- views/dashboard.blade.php --}}
 @extends('layouts.admin')
 
-@section('body-content')
+@section('content')
+<div class="page-title d-flex flex-column justify-content-center flex-wrap me-3 mb-5">
+    <ul class="breadcrumb breadcrumb-separatorless fw-semibold fs-7 my-0 pt-1">
+        <li class="breadcrumb-item text-muted">
+            <a href="" class="text-muted text-hover-primary">Home</a>
+        </li>
+        <li class="breadcrumb-item">
+            <span class="bullet bg-gray-400 w-5px h-2px"></span>
+        </li>
+        <li class="breadcrumb-item text-muted">Dashboards</li>
+    </ul>
+</div>
+<div>
     <h1>Welcome to the Dashboard</h1>
-    <p>This content goes inside the @yield('body-content') spot.</p>
+    <p>This content goes inside the @yield('content') spot.</p>
+</div>
 @endsection
